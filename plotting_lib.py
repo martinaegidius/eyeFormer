a49f2687072032349bebdf385ef2d5871bf9fbf9
Martin Aegidius <gygridd@gmail.com>
2022-10-30 20:55:16 +0100

    init plotting

---
+++ plotting_lib.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 30 20:51:26 2022
+
+@author: max
+"""
+
+import matplotlib.pyplot as plt
+import torch 
+import os 
+
+className = "airplanes"
+path = os.path.dirname(__file__)+"/Data/POETDataset/"+className
+
+#/home/max/Documents/s194119/Bachelor/Data/POETdataset/airplanes/airplanes_test_testresults.pth"
